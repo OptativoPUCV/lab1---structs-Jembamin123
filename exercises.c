51510c734dd95487fe5adde39ecaabb035a32c0a
Replit user <>
2023-08-17 20:32:49 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -53,7 +53,7 @@ int *filterEvenNumbers(int arr[], int size, int *newSize) {
     }
   }
 
-  int *newSize=(int*)malloc(contador*sizeof(int));
+  *newSize=(int*)malloc(contador*sizeof(int));
   
   if(arr==NULL){
     *newSize=0;
